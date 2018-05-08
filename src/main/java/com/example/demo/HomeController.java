4ac81344313ba24afa52e93fc6bdf303dc4d0957
michjens <33859372+michjens@users.noreply.github.com>
2018-05-08 10:32:25 +0200

    getmaps

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,3 +2,32 @@ package com.example.demo;
 
 public class HomeController {
 }
+
+/*  Tilføjer bare skelettet til Menu forsiden for både Mentor og Student
+    @GetMapping("/createTime")  - KUN MENTOR
+    public String Menu(Model model) {
+        //model.addAttribute("", );
+        return "Menu";
+    }
+
+    @GetMapping("/bookTime") - STUDENT
+    public String editInformation(Model model) {
+        model.addAttribute("", );
+        return "bookTime";
+    }
+
+    @GetMapping("/editInformation") - STUDENT + MENTOR
+    public String editInformation(Model model) {
+        model.addAttribute("", );
+        return "editInformation";
+    }
+
+    @GetMapping("/viewUsers") - STUDENT + MENTOR men med forskellig information i postmapping
+    public String viewUsers(Model model) {
+        model.addAttribute("", );
+        return "viewUsers";
+    }
+
+
+
+ */
