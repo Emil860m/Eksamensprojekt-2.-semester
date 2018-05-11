56cdd4c7536da342bd267f1fab9b7188d85702d3
michjens <michael.diksi@hotmail.com>
2018-05-11 11:59:15 +0200

    html

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -3,5 +3,13 @@ package com.example.demo;
 public class Student extends User {
     private String city;                     // Slettet fornavn og efternavn da jeg tænker vi bare nedarver dem fra User
     private String education;
-}                                           /* Slettet "experience" da jeg tænker "3G Allerød Gymnasium"
-                                                   burde dække over både uddannelses niveau, sted og erfaring */
+
+    public Student(String password, String email, int phoneNr, String firstName, String lastName, String city, String experience, String education, String city1, String education1) {
+        super(password, email, phoneNr, firstName, lastName, city, experience, education);
+        this.city = city1;
+        this.education = education1;
+    }
+}
+
+
+
