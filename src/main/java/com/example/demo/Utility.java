b8f263163dddb7b3c070df82cab7bc245fa63012
Emil860m <fenger.emil@gmail.com>
2018-05-11 11:13:08 +0200

    ada

--- src/main/java/com/example/demo/Utility.java
+++ src/main/java/com/example/demo/Utility.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import java.sql.Connection;
-import java.util.Scanner;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,10 +9,19 @@ import java.sql.Statement;
 public class Utility {
 
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DATABASE_URL = "jdbc:mysql://35.195.34.200:3306/ap";
+    static final String DATABASE_URL = "jdbc:mysql://google/ap?cloudSqlInstance=fiery-nimbus-203407:europe-west1:myinstance&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=root&password=yzt42rmt&useSSL=false";
     static Connection con;
 
-    public static void connectDatabase()throws SQLException{
+
+    public static void main(String [] args) throws SQLException, ClassNotFoundException {
+        try {
+            connectDatabase();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void connectDatabase() throws SQLException, ClassNotFoundException {
         try {
 
             //***  Establishing the connection
@@ -27,20 +35,20 @@ public class Utility {
             //*** now that the connection is established we do the query
             s = con.createStatement();
 
-            ResultSet rs = s.executeQuery("SELECT vendor_name,  vendor_city  from vendors where default_account_number > '500'");
+            ResultSet rs = s.executeQuery("SELECT email FROM Users");
 
             // if the resultset is not empty, we position to first row and display first field
             if (rs != null)
                 while (rs.next()) {
                     //System.out.println("Data from name: " + rs.getString("vendor_name") +
                     //      "        " + rs.getString("vendor_city"));
-                    System.out.printf("Data from name: %-34s ",rs.getString("vendor_name"));
-                    System.out.printf("%s\n ",rs.getString("vendor_city"));
+                    System.out.printf("Data from name: %-34s ",rs.getString("Users"));
+                    System.out.printf("%s\n ",rs.getString("email"));
                 }
             s.close();
             con.close();
         }
-        /* correct errorhandling takes up a lot of space */
+        //correct errorhandling takes up a lot of space
         catch (SQLException sqlex) {
             try{
                 System.out.println(sqlex.getMessage());
@@ -53,13 +61,16 @@ public class Utility {
             System.err.println("Driver Class not found");
             System.out.println(noClass.getMessage());
             System.exit(1);  // terminate program
+
         }
+
     }
 
 
 
     //metoden går ud fra at der er en arraylist der hedder users, med alle brugerne i.
     //metoden skal rettes til så den tilgår databasen og skal derefter returnere selve objektet
+    /*
     private static User login(String username, String password) {
         Scanner input = new Scanner(System.in);
 
@@ -71,7 +82,7 @@ public class Utility {
 
     }
 
-
+*/
 
 
 }
